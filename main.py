8327414711107ea6798d7702879c0029872d11db
Jack McPherson <jackemcpherson@gmail.com>
2023-10-11 13:50:00 +1100

    General refactor and clean-up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import List
+from typing import List, Tuple
 
 import dash
 import pandas as pd
@@ -29,56 +29,86 @@ TEAM_LOOKUP = {
 }
 
 
-# Load and sort the DataFrame by 'Total PAV' in descending order
-def get_player_data() -> pd.DataFrame:
-    conn = sqlite3.connect("HPN_Data.db")
-    df = pd.read_sql_query("SELECT * FROM PlayerRatings2023", conn)
-    conn.close()
-    df_sorted = df.sort_values("Total_PAV", ascending=True)
-    return df_sorted
+def load_data_from_db() -> pd.DataFrame:
+    with sqlite3.connect("HPN_Data.db") as conn:
+        return pd.read_sql_query("SELECT * FROM PlayerRatings2023", conn)
 
 
-df_sorted = get_player_data()
+def sort_data_by(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    return df.sort_values(by=column, ascending=True)
+
+
+def create_bar_chart(
+    df: pd.DataFrame, pav_type: str, selected_teams: List[str] = None
+) -> go.Figure:
+    if selected_teams:
+        opacity = [0.6 if tm in selected_teams else 0.1 for tm in df["TM"]]
+        hovertemplate = [
+            f"{player}<br>{pav_value}<br>{tm}" if tm in selected_teams else None
+            for player, pav_value, tm in zip(df["Player"], df[pav_type], df["TM"])
+        ]
+    else:
+        opacity = [0.6] * len(df)
+        hovertemplate = [
+            f"{player}<br>{pav_value}<br>{tm}"
+            for player, pav_value, tm in zip(df["Player"], df[pav_type], df["TM"])
+        ]
+
+    trace = go.Bar(
+        x=df["Player"],
+        y=df[pav_type],
+        name="",
+        marker=dict(opacity=opacity),
+        hovertemplate=hovertemplate,
+        hoverinfo="text",
+        showlegend=False,
+    )
+
+    fig = make_subplots(rows=1, cols=1)
+    fig.add_trace(trace)
+    fig.update_layout(
+        title=f"AFL Player Ratings 2023: {pav_type.replace('_', ' ')}",
+        xaxis_title="Player",
+        xaxis=dict(showticklabels=False),
+        yaxis_title=pav_type.replace("_", " "),
+    )
+
+    return fig
+
 
 app = dash.Dash(__name__)
 
-fig = make_subplots(rows=1, cols=1)
-trace = go.Bar(
-    x=df_sorted["Player"], y=df_sorted["Total_PAV"], name="Total PAV", opacity=1
-)
-fig.add_trace(trace)
+initial_data = load_data_from_db()
+sorted_by_total_pav = sort_data_by(initial_data, "Total_PAV")
+initial_fig = create_bar_chart(sorted_by_total_pav, "Total_PAV")
 
-fig.update_layout(
-    title="AFL Player Ratings 2023: Total PAV",
-    xaxis_title="Player",
-    xaxis=dict(showticklabels=False),
-    yaxis_title="Total PAV",
+team_options = sorted(
+    [
+        {"label": TEAM_LOOKUP[team], "value": team}
+        for team in initial_data["TM"].unique()
+    ],
+    key=lambda x: x["label"],
 )
+pav_options = [
+    {"label": "Total PAV", "value": "Total_PAV"},
+    {"label": "Defensive PAV", "value": "Def_PAV"},
+    {"label": "Offensive PAV", "value": "Off_PAV"},
+    {"label": "Midfield PAV", "value": "Mid_PAV"},
+]
 
 app.layout = html.Div(
     [
-        dcc.Graph(id="bar-chart", figure=fig),
+        dcc.Graph(id="bar-chart", figure=initial_fig),
         dcc.Dropdown(
             id="team-slicer",
-            options=sorted(
-                [
-                    {"label": TEAM_LOOKUP[team], "value": team}
-                    for team in df_sorted["TM"].unique()
-                ],
-                key=lambda x: x["label"],
-            ),
+            options=team_options,
             value=[],
             placeholder="Select Teams",
             multi=True,
         ),
         dcc.Dropdown(
             id="pav-type-selector",
-            options=[
-                {"label": "Total PAV", "value": "Total_PAV"},
-                {"label": "Defensive PAV", "value": "Def_PAV"},
-                {"label": "Offensive PAV", "value": "Off_PAV"},
-                {"label": "Midfield PAV", "value": "Mid_PAV"},
-            ],
+            options=pav_options,
             value="Total_PAV",
             placeholder="Select PAV Type",
             multi=False,
@@ -92,52 +122,9 @@ app.layout = html.Div(
     [Input("team-slicer", "value"), Input("pav-type-selector", "value")],
 )
 def update_bar_chart(selected_teams: List[str], selected_pav_type: str) -> go.Figure:
-    # Sorting dataframe based on the selected PAV type
-    df_sorted_by_pav_type = df_sorted.sort_values(by=selected_pav_type, ascending=True)
-
-    if selected_teams:
-        opacity = [
-            0.6 if tm in selected_teams else 0.1 for tm in df_sorted_by_pav_type["TM"]
-        ]
-        hovertemplate = [
-            f"{player}<br>{pav_value}<br>{tm}" if tm in selected_teams else None
-            for player, pav_value, tm in zip(
-                df_sorted_by_pav_type["Player"],
-                df_sorted_by_pav_type[selected_pav_type],
-                df_sorted_by_pav_type["TM"],
-            )
-        ]
-    else:
-        opacity = [0.6] * len(df_sorted_by_pav_type)
-        hovertemplate = [
-            f"{player}<br>{pav_value}<br>{tm}"
-            for player, pav_value, tm in zip(
-                df_sorted_by_pav_type["Player"],
-                df_sorted_by_pav_type[selected_pav_type],
-                df_sorted_by_pav_type["TM"],
-            )
-        ]
-
-    updated_trace = go.Bar(
-        x=df_sorted_by_pav_type["Player"],
-        y=df_sorted_by_pav_type[selected_pav_type],
-        name="",
-        marker=dict(opacity=opacity),
-        hovertemplate=hovertemplate,
-        hoverinfo="text",
-        showlegend=False,
-    )
-
-    updated_fig = make_subplots(rows=1, cols=1)
-    updated_fig.add_trace(updated_trace)
-    updated_fig.update_layout(
-        title=f"AFL Player Ratings 2023: {selected_pav_type.replace('_', ' ')}",
-        xaxis_title="Player",
-        xaxis=dict(showticklabels=False),
-        yaxis_title=selected_pav_type.replace("_", " "),
-    )
-
-    return updated_fig
+    data = load_data_from_db()
+    sorted_data = sort_data_by(data, selected_pav_type)
+    return create_bar_chart(sorted_data, selected_pav_type, selected_teams)
 
 
 if __name__ == "__main__":
